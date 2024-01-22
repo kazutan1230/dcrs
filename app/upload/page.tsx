b7b3494dd8461cf1e4bd85899d9fc9f1e4c12595
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-01-22 08:49:54 +0000

    uploadFormFeat create.

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,15 +1,11 @@
-import Image from 'next/image'
 // import UploadForm from '@/components/uploadForm'
-import UploadForm_hook from '@/components/uploadForm_hook'
+import { UploadFormHook } from '@/components/uploadFormHook'
 
 export default function Upload() {
-
   return (
     <main className="flex min-h-screen flex-col p-24">
-
       {/* <UploadForm /> */}
-      <UploadForm_hook />
-
+      <UploadFormHook />
     </main>
   )
 }
