68312111261cff803e36f196aef881dc70efdd83
kazutan1230 <145092901+kazutan1230@users.noreply.github.com>
2024-01-30 15:36:50 +0900

    Update components/uploadFormHook.tsx
    Making a space between the ImagePreview and the confirm button.
    
    Co-authored-by: マルコメ <akio.muto@bnt.benextgroup.jp>

--- components/uploadFormHook.tsx
+++ components/uploadFormHook.tsx
@@ -110,6 +110,7 @@ export const UploadFormHook: FC = () => {
             />
           </div>
         </div>
+        <br />
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           type="submit"
