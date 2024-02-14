2ed67825306b4ff060ea5dbfadbc0494b7482c1d
HaradaTakayuki <takayuki.harada@bnt.benextgroup.jp>
2024-02-14 00:21:40 +0000

    トップページのテスト作成

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,21 @@ export default function Home() {
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           障がい者手帳登録システム
         </h2>
-        <div className="mb-8">
-          <p className="text-center text-gray-600">
-            Upload and download your images here
-          </p>
-        </div>
-        <div className="mt-8 space-y-6">
-          <Link
-            href="/upload"
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            [派遣従業員向け]障がい者手帳画像をアップロードします。
-          </Link>
-        </div>
-        <div className="mt-8 space-y-6">
-          <Link
-            href="/download"
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            [労務担当者向け]障がい者手帳画像をダウンロードします。
-          </Link>
-        </div>
+        <p className="text-center text-gray-600">
+          Upload and download your images here
+        </p>
+        <Link
+          href="/upload"
+          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          [派遣従業員向け]障がい者手帳画像をアップロードします。
+        </Link>
+        <Link
+          href="/download"
+          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          [労務担当者向け]障がい者手帳画像をダウンロードします。
+        </Link>
       </div>
     </main>
   )
