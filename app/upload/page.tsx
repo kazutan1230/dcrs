2ed102c78e1f179f53220fd4875611cdc8b512ec
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-14 13:00:02 +0900

    ✨feat: add privacy policy agreement

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -5,10 +5,15 @@ import Link from 'next/link'
 export default function Upload() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-4 py-12 lg:px-8 sm:px-6">
+      <ul className="steps mb-8">
+        <li className="step step-primary">必要情報の入力</li>
+        <li className="step">入力確認</li>
+        <li className="step">完了</li>
+      </ul>
       <h1 className="mb-3 font-semibold text-2xl">必要情報の入力</h1>
-      <span className="before:ml-0.5 before:text-red-500 before:content-['*']">
+      <p className="before:ml-0.5 before:text-red-500 before:content-['*']">
         は必須項目
-      </span>
+      </p>
       <br />
       <ProfileForm />
       <br />
