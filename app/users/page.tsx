811b3f6270fce58f12a4981c54b49eb9b15e45c2
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-29 09:32:38 +0900

    🆙bump: update dependencies

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,7 @@
 import type { User } from '@prisma/client'
 
 function getUsers() {
-  return fetch(`${process.env.URL}/api/user`)
+  return fetch(`${process.env.URL}/api/user`, { next: { revalidate: 300 } })
     .then((res) => {
       if (!res.ok) {
         return null
