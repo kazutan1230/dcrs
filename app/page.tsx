a37acbb5ce11eb916282a5b982be98e8a24463b9
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-04 15:52:10 +0900

    🎨style: change theme icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { getUsers } from '@/app/lib/getUsers'
-import { CameraIcon, PlusIcon, TableCellsIcon } from '@heroicons/react/24/solid'
+import {
+  CameraIcon,
+  CheckCircleIcon,
+  HandThumbUpIcon,
+  PencilSquareIcon,
+  PlusIcon,
+  TableCellsIcon,
+} from '@heroicons/react/24/solid'
 import type { User } from '@prisma/client'
 import Link from 'next/link'
 import type React from 'react'
@@ -15,7 +22,33 @@ export default function Home(): React.JSX.Element {
         <br />
         登録システム
       </h1>
-      <br />
+      <ul className="timeline timeline-vertical lg:timeline-horizontal">
+        <li>
+          <div className="timeline-start">1</div>
+          <div className="timeline-middle">
+            <PencilSquareIcon className="size-5" />
+          </div>
+          <div className="timeline-end timeline-box">必要情報の入力</div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-start">2</div>
+          <div className="timeline-middle">
+            <CheckCircleIcon className="size-5" />
+          </div>
+          <div className="timeline-end timeline-box">入力確認</div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-start">3</div>
+          <div className="timeline-middle">
+            <HandThumbUpIcon className="size-5" />
+          </div>
+          <div className="timeline-end timeline-box">完了</div>
+        </li>
+      </ul>
       <Link href="/register" className="btn btn-primary hover:scale-110">
         <CameraIcon className="size-6" />
         障がい者手帳画像を提出
