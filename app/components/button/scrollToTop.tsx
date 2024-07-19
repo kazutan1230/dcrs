ffee9e988808e0ed56e2a8fa3251aa6f97628a88
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-07-19 13:39:19 +0900

    ♻️refactor: scrollToTop function

--- app/components/button/scrollToTop.tsx
+++ app/components/button/scrollToTop.tsx
@@ -1,35 +1,41 @@
 "use client"
 
 import { ChevronDoubleUpIcon } from "@heroicons/react/24/solid"
-import { useEffect, useRef, useState } from "react"
+import { type RefObject, useEffect, useRef, useState } from "react"
 import type React from "react"
 
 export function ScrollToTop(): React.JSX.Element {
-  const ref: React.RefObject<HTMLButtonElement> =
-    useRef<HTMLButtonElement>(null)
+  const ref: RefObject<HTMLButtonElement> = useRef<HTMLButtonElement>(null)
   const scrollBtn: HTMLButtonElement = ref.current as HTMLButtonElement
   const [scrollY, setScrollY] = useState<number>(0)
   const [isScrollDown, setIsScrollDown] = useState<boolean>(false)
   const scrollPoint: number = 200
 
-  if (typeof window !== "undefined") {
-    window.addEventListener("scroll", () => setScrollY(window.scrollY))
-  }
-
   useEffect(() => {
-    if (scrollPoint < scrollY) {
+    window.addEventListener("scroll", () => setScrollY(window.scrollY))
+    if (
+      scrollPoint < scrollY &&
+      !scrollBtn.classList.contains("scroll-to-top")
+    ) {
+      scrollBtn.classList.remove("hidden")
+      scrollBtn.classList.remove("fade-out-down")
+      scrollBtn.classList.add("fade-in-up")
       setIsScrollDown(true)
     }
     if (scrollY < scrollPoint && isScrollDown) {
       scrollBtn.classList.remove("fade-in-up")
-      scrollBtn.classList.remove("hidden")
+      scrollBtn.classList.remove("scroll-to-top")
       scrollBtn.classList.add("fade-out-down")
+    }
+    return () => {
+      window.removeEventListener("scroll", () => setScrollY(window.scrollY))
     }
   })
 
-  function scrollToTop() {
+  function scrollToTop(): void {
     scrollBtn.classList.remove("fade-in-up")
     scrollBtn.classList.add("fade-out-down")
+    scrollBtn.classList.add("scroll-to-top")
     window.scrollTo({ top: 0, behavior: "smooth" })
   }
 
@@ -37,9 +43,7 @@ export function ScrollToTop(): React.JSX.Element {
     <button
       type="button"
       ref={ref}
-      className={`btn btn-square btn-primary fixed right-5 bottom-5 gap-0 hover:scale-110 ${
-        scrollY < scrollPoint ? "hidden" : "fade-in-up"
-      }`}
+      className="btn btn-square btn-primary hidden fixed bottom-5 right-5 gap-0 hover:scale-110"
       onClick={scrollToTop}
     >
       <ChevronDoubleUpIcon className="size-8 scroll-up" />
