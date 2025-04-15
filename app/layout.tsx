866b72bf071bd796baaabac99941519c180783f2
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-04-15 15:44:28 +0900

    🚀feat: use next font variable (#931)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { Header } from "@/app/components/layout/header"
 import { NavigationBlockerProvider } from "@/app/components/layout/navigationBlocker"
 import { SITE_TITLE } from "@/app/lib/constant"
 import type { Metadata } from "next"
-import type { NextFont } from "next/dist/compiled/@next/font"
 import { Sawarabi_Gothic } from "next/font/google"
 import "./globals.css"
 import { SessionProvider } from "next-auth/react"
@@ -15,9 +14,10 @@ import {
   unstable_ViewTransition as ViewTransition,
 } from "react"
 
-const sawarabi: NextFont = Sawarabi_Gothic({
+const sawarabi = Sawarabi_Gothic({
   subsets: ["latin"],
   weight: "400",
+  variable: "--font-sawarabi",
 })
 
 export const metadata: Metadata = {
@@ -30,8 +30,8 @@ export default function RootLayout({
   modal,
 }: Readonly<{ children: ReactNode; modal: JSX.Element }>): JSX.Element {
   return (
-    <html lang="ja">
-      <body className={sawarabi.className}>
+    <html lang="ja" className={sawarabi.variable}>
+      <body className="font-sawarabi">
         <ViewTransition>
           <SessionProvider>
             <NavigationBlockerProvider>
